a59f9941d19e97009a138dbbc1f65afcfe6f149a
Oluwadabira Akinwumi <dabira.akinwumi@gmail.com>
2023-08-16 01:00:40 -0400

    ii

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_08_15_000601) do
-  create_table "Friends", force: :cascade do |t|
+  create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "email"
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_000601) do
     t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
-  create_table "Users", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
